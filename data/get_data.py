18b5733caa0b6e8a0eedf48812d3988615404479
mahapsub <smahapat7@gmail.com>
2019-01-28 13:25:13 -0600

    adding a get_data function to download 2013 and 2015 datasets in the appropriate directory.
    --wget library added
    --zipfile library added

---
+++ data/get_data.py
@@ -0,0 +1,19 @@
+import wget
+from zipfile import ZipFile
+
+print('Beginning file download with wget module')
+
+
+#urls for 2013 and 2015 tweets
+urls = ['https://canvas.northwestern.edu/courses/88875/files/6315645/download?verifier=XdmSiOr2uwWs0abm4upCcqWdZDmbCc9KzqVrzZRP&wrap=1',
+        'https://canvas.northwestern.edu/courses/88875/files/6315652/download?verifier=oetjl6vjvBWVWYzALCQR7M2E0CdTMtwnt5U7gcei&wrap=1']
+
+for u in urls:
+    filename = wget.download(u)
+
+    zip = ZipFile(filename)
+    zip.printdir()
+    zip.extractall()
+    print(filename, '--extract succesfull')
+
+
